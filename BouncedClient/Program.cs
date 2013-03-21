9ced79850174d5b06304f106a4d16f37c07d9520
Raghav Sethi <raghavsethi.rs@gmail.com>
2013-03-22 01:24:04 +0530

    Fixed multiple instance bug

--- BouncedClient/Program.cs
+++ BouncedClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic.ApplicationServices; // To ensure single-instance
 
 namespace BouncedClient
 {
@@ -16,7 +17,30 @@ namespace BouncedClient
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            SingleInstanceController controller = new SingleInstanceController();
+            controller.Run(new string[0]);
+            //Application.Run(new MainForm());
+        }
+    }
+
+    public class SingleInstanceController : WindowsFormsApplicationBase
+    {
+        public SingleInstanceController()
+        {
+            IsSingleInstance = true;
+
+            StartupNextInstance += this_StartupNextInstance;
+        }
+
+        void this_StartupNextInstance(object sender, StartupNextInstanceEventArgs e)
+        {
+            MainForm form = MainForm as MainForm;
+            form.Show();
+        }
+
+        protected override void OnCreateMainForm()
+        {
+            MainForm = new MainForm();
         }
     }
 }
